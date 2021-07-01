117081635272c71cc6638006fab8b4f551f94651
Veri97 <veri.kazazi1997@hotmail.com>
2021-07-01 19:07:40 +0200

    implemented Update and Delete event functionalities

---
+++ TicketApp.TicketManagement/TicketApp.TicketManagement.Application/Features/Events/Commands/DeleteEvent/DeleteEventCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketApp.TicketManagement.Application.Contracts.Persistence;
+using TicketApp.TicketManagement.Domain.Entities;
+
+namespace TicketApp.TicketManagement.Application.Features.Events.Commands.DeleteEvent
+{
+    public class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommand>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteEventCommandHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+        {
+            var eventToDelete = await _eventRepository.GetByIdAsync(request.EventId);
+
+            await _eventRepository.DeleteAsync(eventToDelete);
+
+            return Unit.Value;
+        }
+    }
+}

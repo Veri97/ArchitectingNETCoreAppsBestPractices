2b8679b623bbf4d6d910fa6a88064a228291962c
Veri97 <veri.kazazi1997@hotmail.com>
2021-07-06 19:10:50 +0200

    added DbContext class for database code-first creation , with seed data

---
+++ TicketApp.TicketManagement/TicketApp.TicketManagement.Persistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketApp.TicketManagement.Domain.Entities;
+
+namespace TicketApp.TicketManagement.Persistence.Configurations
+{
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}

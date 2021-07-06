04011513ad308a1cdd2565bb57564f21cc854db1
Veri97 <veri.kazazi1997@hotmail.com>
2021-07-06 21:55:51 +0200

    created extension method for registering persistence services

---
+++ TicketApp.TicketManagement/TicketApp.TicketManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketApp.TicketManagement.Application.Contracts.Persistence;
+using TicketApp.TicketManagement.Persistence.Repositories;
+
+namespace TicketApp.TicketManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services
+            ,IConfiguration configuration)
+        {
+            services.AddDbContext<TicketAppDbContext>(options =>
+              options.UseSqlServer(configuration.GetConnectionString("TicketManagementConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+
+            return services;
+        }
+    }
+}

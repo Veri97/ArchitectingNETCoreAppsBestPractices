47b928e52bd365ed9511f74ce26a343cce4e8bd1
Veri97 <veri.kazazi1997@hotmail.com>
2021-07-06 22:59:22 +0200

    finished EmailService implementation and added extension method to register this service

---
+++ TicketApp.TicketManagement/TicketApp.TicketManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketApp.TicketManagement.Application.Contracts.Infrastructure;
+using TicketApp.TicketManagement.Application.Models.Mail;
+using TicketApp.TicketManagement.Infrastructure.Mail;
+
+namespace TicketApp.TicketManagement.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
---
+++ TicketApp.TicketManagement/TicketApp.TicketManagement.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketApp.TicketManagement.Application.Contracts.Infrastructure;
+using TicketApp.TicketManagement.Application.Models.Mail;
+
+namespace TicketApp.TicketManagement.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSettings _emailSettings { get; }
+
+        public EmailService(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var body = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, body, body);
+            var response = await client.SendEmailAsync(sendGridMessage, new CancellationToken());
+
+            return response.StatusCode == HttpStatusCode.Accepted 
+                   || response.StatusCode == HttpStatusCode.OK;
+        }
+    }
+}

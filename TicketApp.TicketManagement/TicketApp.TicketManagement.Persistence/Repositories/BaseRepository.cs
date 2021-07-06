78d293fe15e75579d424b432a88faebdfc0d1951
Veri97 <veri.kazazi1997@hotmail.com>
2021-07-06 19:23:59 +0200

    base repository implementation

---
+++ TicketApp.TicketManagement/TicketApp.TicketManagement.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TicketApp.TicketManagement.Application.Contracts.Persistence;
+
+namespace TicketApp.TicketManagement.Persistence.Repositories
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : class
+    {
+        protected readonly TicketAppDbContext _dbContext;
+        public BaseRepository(TicketAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}

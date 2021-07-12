7743b24fdd43c15946081ad3fb07af351e87406a
Veri97 <veri.kazazi1997@hotmail.com>
2021-07-12 23:16:45 +0200

    add Category controller and complete missing automapper profile registrations

--- TicketApp.TicketManagement.Application/Profiles/MappingProfile.cs
+++ TicketApp.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using TicketApp.TicketManagement.Application.Features.Categories.Commands.CreateCateogry;
 using TicketApp.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
 using TicketApp.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
 using TicketApp.TicketManagement.Application.Features.Events.Commands.CreateEvent;
@@ -14,14 +15,17 @@ namespace TicketApp.TicketManagement.Application.Profiles
         public MappingProfile()
         {
             CreateMap<Event, EventListVm>().ReverseMap();
-            CreateMap<Event, EventDetailVm>().ReverseMap();
-            CreateMap<Category, CategoryDto>();
-
-            CreateMap<Category, CategoryListVm>();
-            CreateMap<Category, CategoryEventListVm>();
-
             CreateMap<Event, CreateEventCommand>().ReverseMap();
             CreateMap<Event, UpdateEventCommand>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Event, CategoryEventDto>().ReverseMap();
+
+            CreateMap<Category, CategoryDto>();
+            CreateMap<Category, CategoryListVm>();
+            CreateMap<Category, CategoryEventListVm>();
+            CreateMap<Category, CreateCategoryCommand>();
+            CreateMap<Category, CreateCategoryDto>();
+
 
         }
     }

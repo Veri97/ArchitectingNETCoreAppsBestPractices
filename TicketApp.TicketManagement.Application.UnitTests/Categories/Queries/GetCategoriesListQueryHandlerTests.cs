ca5ca7ecc16a8d24e45cb55ed217ff7b79dc1491
Veri97 <veri.kazazi1997@hotmail.com>
2021-07-17 20:42:53 +0200

    add test for GetCategoriesListQueryHandler

---
+++ TicketApp.TicketManagement.Application.UnitTests/Categories/Queries/GetCategoriesListQueryHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketApp.TicketManagement.Application.Contracts.Persistence;
+using TicketApp.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using TicketApp.TicketManagement.Application.Profiles;
+using TicketApp.TicketManagement.Domain.Entities;
+using Xunit;
+
+namespace TicketApp.TicketManagement.Application.UnitTests.Categories.Queries
+{
+    public class GetCategoriesListQueryHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IAsyncRepository<Category>> _mockCategoryRepository;
+
+        public GetCategoriesListQueryHandlerTests()
+        {
+            _mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetCategoriesListTest()
+        {
+            var handler = new GetCategoriesListQueryHandler(_mapper, _mockCategoryRepository.Object);
+
+            var result = await handler.Handle(new GetCategoriesListQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<CategoryListVm>>();
+    
+            result.Count.ShouldBe(4);
+        }
+
+    }
+}
